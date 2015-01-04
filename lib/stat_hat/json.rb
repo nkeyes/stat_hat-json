bbe45d38ca5d3a0ff73f9d9947d986590be7168a
Nathan Keyes <nathan.keyes@avantcredit.com>
2015-01-04 11:38:15 -0600

    initial commit

---
+++ lib/stat_hat/json.rb
@@ -0,0 +1,7 @@
+require "stat_hat/json/version"
+
+module StatHat
+  module Json
+    # Your code goes here...
+  end
+end
---
+++ lib/stat_hat/json/version.rb
@@ -0,0 +1,5 @@
+module StatHat
+  module Json
+    VERSION = "0.0.1"
+  end
+end
